32c32ab6c007b378d6eec5c277955230b3cbd628
TuurBellersDAE <tuur.bellers@student.howest.be>
2025-03-03 01:56:52 +0100

    Done imgui

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,6 +1,12 @@
 #include <SDL.h>
 #include "InputManager.h"
 
+
+#include "imgui.h"
+#include "backends/imgui_impl_opengl3.h"   //Don`t know why i cant just #include "imgui_impl_opengl3.h"
+#include "backends/imgui_impl_sdl2.h"      //Don`t know why i cant just #include "imgui_impl_sdl2.h
+
+
 bool dae::InputManager::ProcessInput()
 {
 	SDL_Event e;
@@ -16,6 +22,8 @@ bool dae::InputManager::ProcessInput()
 			
 		}
 		// etc...
+
+		ImGui_ImplSDL2_ProcessEvent(&e);
 	}
 
 	return true;
